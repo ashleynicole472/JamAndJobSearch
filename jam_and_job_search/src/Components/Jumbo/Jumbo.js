85dc5bb774e3f8f99cbfdfd13977c66257cadaa8
ashleynicole472 <asha.4720@gmail.com>
2018-06-05 16:42:21 -0700

    Adjusted wording on Jumbotron and padding

--- jam_and_job_search/src/Components/Jumbo/Jumbo.js
+++ jam_and_job_search/src/Components/Jumbo/Jumbo.js
@@ -7,13 +7,16 @@ class Jumbo extends Component {
             <div className="row">
                 <div className="col-lg-12 col-sm-12">
                     <div className="jumbotron classes">
-                        <h1 className="display-3">Hello and Welcome!</h1>
-                        <p className="lead">Here you can get in the zone by listening to your favorite artists while looking for your next new carrer.</p>
-                        <p className="lead">Scroll down to find new jobs in your area or anywhere youre wanting to work next. </p>
-                        <p className="lead">Below you are able to explore featured artist, when artist will be in your area, and listen to your favorite station on Spotify. </p>
+                        <br />
+                        <br />
+                        <h3 className="display-3">Jam & Search!</h3>
+                        <h2 className="display-5">Designed to help you enjoy job searching</h2>
                         <hr className="my-4" />
+                        <h6 className="lead">Here you can get in the zone by listening to your favorite artists while looking for your next new carrer.<br />
+                        Designed to be a one stop shop for those who cant just search without some amazing music in the background.</h6>
+                        <p></p>
                         <p className="lead">
-                        <a className="btn btn-info btn-lg" href="#" role="button">Learn more</a>
+                        <a className="btn btn-info btn-lg" href="#" role="button">Happy searching!</a>
                         </p>
                     </div>
                 </div>
