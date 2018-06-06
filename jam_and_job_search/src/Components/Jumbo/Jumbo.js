f9bbe6dc52e167be3462b287f0cb027e29155af8
ashleynicole472 <asha.4720@gmail.com>
2018-06-05 19:43:55 -0700

    Added react-router

--- jam_and_job_search/src/Components/Jumbo/Jumbo.js
+++ jam_and_job_search/src/Components/Jumbo/Jumbo.js
@@ -1,4 +1,10 @@
 import React, { Component } from 'react';
+import {
+  BrowserRouter as Router,
+  StaticRouter, // for server rendering
+  Route,
+  Link
+} from 'react-router-dom'
 import './Jumbo.css';
 
 class Jumbo extends Component {
@@ -13,10 +19,13 @@ class Jumbo extends Component {
                         <h2 className="display-5">Designed to help you enjoy job searching</h2>
                         <hr className="my-4" />
                         <h6 className="lead">Here you can get in the zone by listening to your favorite artists while looking for your next new carrer.<br />
-                        Designed to be a one stop shop for those who cant just search without some amazing music in the background.</h6>
+                        Designed to be a one stop shop for those who cant just search without having some amazing music in the background.</h6>
                         <p></p>
                         <p className="lead">
-                        <a className="btn btn-info btn-lg" href="#" role="button">Happy searching!</a>
+                        <Router>
+                            <Link to="./JobSearchPage" className="btn btn-info btn-lg"
+                            role="button">Happy searching!</Link>
+                        </Router>
                         </p>
                     </div>
                 </div>
