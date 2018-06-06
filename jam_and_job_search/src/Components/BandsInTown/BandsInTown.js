7a988c0bbb358f5671eb9ac1df9a6cb9b8ce182e
ashleynicole472 <asha.4720@gmail.com>
2018-06-06 12:13:29 -0700

    Added scroll area for when artist load under artist search area

--- jam_and_job_search/src/Components/BandsInTown/BandsInTown.js
+++ jam_and_job_search/src/Components/BandsInTown/BandsInTown.js
@@ -39,16 +39,17 @@ class BandsInTown extends Component {
         .then((data) => {
             console.log(data);
             const artistInfo = data.map((element) =>
-            <tr className="table-primary">
+            <tr className="table-primary" key={element.id}>
                 <td>{element.lineup}</td>
                 <td>{element.venue.city}</td>
                 <td>{element.venue.name}</td>
                 <td>{moment(element.datetime).format('llll')}</td>
                 <td>
-                    <span className="badge badge-pill badge-info"
+                    <button
                         type="button"
-                        href={element.url} target="#">Buy Tickets
-                    </span>
+                        className="btn btn-primary btn-sm"
+                        href={element.offers.url} target="#">Buy Tickets
+                    </button>
                 </td>
             </tr>
         )
@@ -67,7 +68,7 @@ class BandsInTown extends Component {
             <div className="container">
                 <div className="row">
                     <div className="col-md-6">
-                        <h2>Search Artist with Bands In Town<i class="far fa-hand-rock"></i></h2>
+                        <h2 className="bandsintown">Search Artist with Bands In Town<i className="far fa-hand-rock"></i></h2>
                     <div className="form-group">
                         <label className="col-form-label col-form-label-lg" for="inputLarge">Find when theyll be in your area!</label>
                         <input
@@ -84,24 +85,26 @@ class BandsInTown extends Component {
                                 type="button"
                                 className="btn btn-info btn-md btn-block">Search
                             </button>
+                            <div className="bands">
                             {this.state.isDownloaded ?
-                            <table>
-                                <tbody>
-                                    <tr className="table-primary">
-                                    <th scope="row">Artist</th>
-                                    <th scope="row">City</th>
-                                    <th scope="row">Venue</th>
-                                    <th scope="row">Date and Time</th>
-                                    <th scope="row">Tickets</th>
-                                    </tr>
-                                    {this.state.artistSearchResults}
-                                </tbody>
-                            </table> : null}
+                                <table>
+                                    <tbody>
+                                        <tr className="table-primary">
+                                        <th scope="row">Artist</th>
+                                        <th scope="row">City</th>
+                                        <th scope="row">Venue</th>
+                                        <th scope="row">Date and Time</th>
+                                        <th scope="row">Tickets</th>
+                                        </tr>
+                                        {this.state.artistSearchResults}
+                                    </tbody>
+                                </table> : null}
+                            </div>
                         </div>
                     </div>
                 </div>
                     <div className="col-md-6">
-                        <h2>Search for music on Spotify <i class="fab fa-spotify"></i></h2>
+                        <h2 className="spotify">Search for music on Spotify <i className="fab fa-spotify"></i></h2>
                         <div className="form-group">
                             <label className="col-form-label col-form-label-lg" for="inputLarge">Enjoy listening to your fav music while job searching!</label>
                             <input
@@ -112,16 +115,14 @@ class BandsInTown extends Component {
                                 id="inputLarge" />
                         </div>
                         <div className="row">
-                            <div className="col-md-4">
-                            </div>
-                            <div className="col-md-4">
+
+                            <div className="col-md-12">
                                 <button
                                     type="button"
                                     className="btn btn-info btn-md btn-block">Search
                                 </button>
                             </div>
-                            <div className="col-md-4">
-                            </div>
+
                         </div>
                     </div>
                 </div>
